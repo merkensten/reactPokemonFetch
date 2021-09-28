3a793460ef13b8a59b51418c6a5f8ee9b7014e1d
Mattis <m.erkensten@icloud.com>
2021-09-28 22:01:15 +0200

    Git init och lätt sturktur skapad

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { Title } from './components/title/Title';
+import { SearchInput } from './components/search-input/Search-input';
+import './styles/main.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
+      <SearchInput />
     </div>
   );
 }
---
+++ src/components/search-input/Search-input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Search-input.css'
+
+export const SearchInput = () => {
+    return (
+        <>
+            <h2>Sök efter en pokemon</h2>
+            <input type="text" placeholder="Sök efter en pokemon" />
+        </>
+    )
+}
