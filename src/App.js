85cdeb87550d3d73139ba0ef3c5d4275393cfaf4
Mattis <m.erkensten@icloud.com>
2021-10-03 19:09:38 +0200

    Simpel version skapad

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
-import { Title } from './components/title/Title';
-import { SearchInput } from './components/search-input/Search-input';
-import './styles/main.css'
+import { useState } from 'react';
+import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonChosen, setPokemonChosen] = useState(false);
+  const [pokemon, setPokemon] = useState({
+    name: '',
+    species: '',
+    image: '',
+    hp: '',
+    attack: '',
+    defense: '',
+    type: '',
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then(
+      (response) => {
+        setPokemon({
+          name: pokemonName,
+          species: response.data.species.name,
+          image: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+      }
+    );
+  };
+
   return (
     <div className="App">
-      <Title />
-      <SearchInput />
+      <div className="TitleSection">
+        <h1>Pokemon Stats</h1>
+        <input
+          type="text"
+          placeholder="Search Pokemon"
+          onChange={(event) => {
+            setPokemonName(event.target.value);
+          }}
+        />
+        <button onClick={searchPokemon}>Search Pokemon</button>
+      </div>
+      <div className="DisplaySection">
+        {!pokemonChosen ? (
+          <h1>Please chose a Pokemon</h1>
+        ) : (
+          <>
+            <h1>{pokemon.name}</h1>
+            <img src={pokemon.image} alt={pokemon.name} />
+            <h3>Species: {pokemon.species}</h3>
+            <h3>Type: {pokemon.type}</h3>
+            <h4>Hp: {pokemon.hp}</h4>
+            <h4>Attack: {pokemon.attack}</h4>
+            <h4>Defense: {pokemon.defense}</h4>
+          </>
+        )}
+      </div>
     </div>
   );
 }
